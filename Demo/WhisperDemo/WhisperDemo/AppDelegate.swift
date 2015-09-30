f8c141ccd8641a08f21c88314dc58baf257200fc
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-30 17:16:42 +0200

    Adding the basic configuration of the demo.

--- Demo/WhisperDemo/WhisperDemo/AppDelegate.swift
+++ Demo/WhisperDemo/WhisperDemo/AppDelegate.swift
@@ -5,8 +5,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+  lazy var navigationController: UINavigationController = {
+    let navigationController = UINavigationController(rootViewController: ViewController())
+    
+    return navigationController
+    }()
+
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    window?.rootViewController = ViewController()
+    window = UIWindow()
+    window?.rootViewController = navigationController
     window?.makeKeyAndVisible()
     
     return true
--- Demo/WhisperDemo/WhisperDemo/ViewController.swift
+++ Demo/WhisperDemo/WhisperDemo/ViewController.swift
@@ -2,9 +2,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  lazy var icon: UIImageView = {
+    let imageView = UIImageView()
+    return imageView
+    }()
+
+  lazy var titleLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Welcome to the magic of a tiny Whisper... 🍃"
+    label.font = UIFont(name: "HelveticaNeue-Medium", size: 20)!
+    label.textColor = UIColor(red:0.86, green:0.86, blue:0.86, alpha:1)
+    label.textAlignment = .Center
+    label.numberOfLines = 0
+    label.frame.size.width = UIScreen.mainScreen().bounds.width - 60
+    label.sizeToFit()
+
+    return label
+    }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = UIColor.redColor()
+
+    title = "Whisper".uppercaseString
+    view.backgroundColor = UIColor.whiteColor()
+
+    for subview in [icon, titleLabel] { view.addSubview(subview) }
+
+    setupFrames()
+  }
+
+  // MARK - Configuration
+
+  func setupFrames() {
+    let totalSize = UIScreen.mainScreen().bounds
+
+    titleLabel.frame.origin = CGPoint(x: (totalSize.width - titleLabel.frame.width) / 2, y: totalSize.height / 2)
   }
 }
 
