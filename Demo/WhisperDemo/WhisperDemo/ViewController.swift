c20fee7d85f38a257977fa7843b5a7db7ed4befd
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-30 16:40:35 +0200

    Adds the main project demo structure.

---
+++ Demo/WhisperDemo/WhisperDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhisperDemo
+//
+//  Created by Ramon Gilabert Llop on 9/30/15.
+//  Copyright © 2015 Ramon Gilabert Llop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
