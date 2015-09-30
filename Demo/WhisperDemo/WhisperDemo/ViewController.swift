cd792154356e36de259872f04f88aaeca6787eeb
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-30 16:47:41 +0200

    Adds basic configuration.

--- Demo/WhisperDemo/WhisperDemo/ViewController.swift
+++ Demo/WhisperDemo/WhisperDemo/ViewController.swift
@@ -1,25 +1,10 @@
-//
-//  ViewController.swift
-//  WhisperDemo
-//
-//  Created by Ramon Gilabert Llop on 9/30/15.
-//  Copyright © 2015 Ramon Gilabert Llop. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    view.backgroundColor = UIColor.redColor()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
