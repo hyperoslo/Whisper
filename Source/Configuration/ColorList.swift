4799ddf939561127e2fa345071be68086f371c94
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-16 16:00:30 +0100

    Adds a font list

---
+++ Source/Configuration/ColorList.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+public struct ColorList {
+
+  public struct Shout {
+
+    public static var background = UIColor.whiteColor()
+    public static var dragIndicator = UIColor.lightGrayColor()
+    public static var title = UIColor.blackColor()
+    public static var subtitle = UIColor.blackColor()
+  }
+}
