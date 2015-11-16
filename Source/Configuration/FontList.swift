935d4df5b0543e286f4cb0fabe9d455aa7d90f6b
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-16 16:01:17 +0100

    Adds some constants to the shout font list

--- Source/Configuration/FontList.swift
+++ Source/Configuration/FontList.swift
@@ -2,4 +2,9 @@ import UIKit
 
 public struct FontList {
 
+  public struct Shout {
+
+    public static let title = UIFont.boldSystemFontOfSize(14)
+    public static let subtitle = UIFont.systemFontOfSize(12)
+  }
 }
