9832bfe16adc00ca30816578becececc1fd2871a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-12-02 13:21:48 +0100

    Adds some of the core methods

--- Source/Message.swift
+++ Source/Message.swift
@@ -36,11 +36,13 @@ public struct Murmur {
   public var duration: NSTimeInterval
   public var backgroundColor: UIColor
   public var titleColor: UIColor
+  public var font: UIFont
 
-  public init(title: String, duration: NSTimeInterval = 1.5, backgroundColor: UIColor = ColorList.Whistle.background, titleColor: UIColor = ColorList.Whistle.title) {
+  public init(title: String, duration: NSTimeInterval = 1.5, backgroundColor: UIColor = ColorList.Whistle.background, titleColor: UIColor = ColorList.Whistle.title, font: UIFont = FontList.Whistle.title) {
     self.title = title
     self.duration = duration
     self.backgroundColor = backgroundColor
     self.titleColor = titleColor
+    self.font = font
   }
 }
--- Source/WhistleFactory.swift
+++ Source/WhistleFactory.swift
@@ -1,13 +1,21 @@
 import UIKit
 
-Whistle()
+let whitleFactory = WhistleFactory()
 
-public class Whistle: UIView {
+public func Whistle(murmur: Murmur) {
+  whistleFactory.configureWhistle(murmur)
+}
+
+public class WhistleFactory: UIView {
 
   public lazy var titleLabel: UILabel = {
     let label = UILabel()
     return label
     }()
+
+  public var duration: NSTimeInterval = 2
+
+  // MARK: - Initializers
 
   public override init(frame: CGRect) {
     super.init(frame: frame)
@@ -15,7 +23,36 @@ public class Whistle: UIView {
     addSubview(titleLabel)
   }
 
-  public func configureWhistle() {
+  public required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Configuration
+
+  public func configureWhistle(murmur: Murmur) {
+    titleLabel.text = murmur.title
+    titleLabel.font = murmur.font
+    titleLabel.textColor = murmur.titleColor
+    backgroundColor = murmur.backgroundColor
+
+    setupFrames()
+  }
+
+  // MARK: - Setup
+
+  public func setupFrames() {
+    titleLabel.sizeToFit()
+
+
+  }
+
+  // MARK: - Movement methods
+
+  public func present() {
+
+  }
+
+  public func hide() {
     
   }
 }
