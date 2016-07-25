15d3e65da0db6ed821d50978b43ef9c3fb439a48
Khoa Pham <onmyway133@gmail.com>
2016-07-25 14:30:42 +0200

    Add show and hide fuctions

---
+++ Source/Showing.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+public func show(whisper message: Message, to: UINavigationController, action: WhisperAction = .Present) {
+  whisperFactory.craft(message, navigationController: to, action: action)
+}
+
+public func hide(whisper from: UINavigationController, after: NSTimeInterval = 0) {
+  whisperFactory.silentWhisper(from, after: after)
+}
+
+public func show(shout announcement: Announcement, to: UIViewController, completion: (() -> Void)? = nil) {
+  shoutView.craft(announcement, to: to, completion: completion)
+}
+
+public func show(whistle murmur: Murmur, action: WhistleAction = .Present) {
+  whistleFactory.whistler(murmur, action: action)
+}
+
+public func hide(whistle after: NSTimeInterval = 0) {
+  whistleFactory.calm(after: after)
+}
