d0a98734b30c5929b7ea878f23c9364f1f7136f8
Konrad Zdunczyk <konrad.zdunczyk@grupawp.pl>
2016-08-01 17:19:52 +0200

    Showing namespace added

--- Source/Showing.swift
+++ Source/Showing.swift
@@ -1,21 +1,24 @@
 import UIKit
 
-public func show(whisper message: Message, to: UINavigationController, action: WhisperAction = .Present) {
-  whisperFactory.craft(message, navigationController: to, action: action)
+public enum Showing {
+    public static func show(whisper message: Message, to: UINavigationController, action: WhisperAction = .Present) {
+        whisperFactory.craft(message, navigationController: to, action: action)
+    }
+
+    public static func hide(whisperFrom from: UINavigationController, after: NSTimeInterval = 0) {
+        whisperFactory.silentWhisper(from, after: after)
+    }
+
+    public static func show(shout announcement: Announcement, to: UIViewController, completion: (() -> Void)? = nil) {
+        shoutView.craft(announcement, to: to, completion: completion)
+    }
+
+    public static func show(whistle murmur: Murmur, action: WhistleAction = .Present) {
+        whistleFactory.whistler(murmur, action: action)
+    }
+
+    public static func hide(whistleAfter after: NSTimeInterval = 0) {
+        whistleFactory.calm(after: after)
+    }
 }
 
-public func hide(whisperFrom from: UINavigationController, after: NSTimeInterval = 0) {
-  whisperFactory.silentWhisper(from, after: after)
-}
-
-public func show(shout announcement: Announcement, to: UIViewController, completion: (() -> Void)? = nil) {
-  shoutView.craft(announcement, to: to, completion: completion)
-}
-
-public func show(whistle murmur: Murmur, action: WhistleAction = .Present) {
-  whistleFactory.whistler(murmur, action: action)
-}
-
-public func hide(whistleAfter after: NSTimeInterval = 0) {
-  whistleFactory.calm(after: after)
-}
