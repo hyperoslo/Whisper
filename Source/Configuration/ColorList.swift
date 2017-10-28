49258a8d48f1f5345f8617b169ce10e352938ae4
Baimaikp <baimaikhawpheak@gmail.com>
2017-10-28 23:37:46 +0700

    Update ColorList.swift

--- Source/Configuration/ColorList.swift
+++ Source/Configuration/ColorList.swift
@@ -1,14 +1,14 @@
 import UIKit
-
+//Manage Color
 public struct ColorList {
-
+//Set color of Shout
   public struct Shout {
     public static var background = UIColor.white
     public static var dragIndicator = UIColor(red:0.90, green:0.90, blue:0.90, alpha:1)
     public static var title = UIColor.black
     public static var subtitle = UIColor.black
   }
-
+//Set color of whistle
   public struct Whistle {
     public static var background = UIColor.white
     public static var title = UIColor.black
